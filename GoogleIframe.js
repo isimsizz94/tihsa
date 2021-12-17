6cb2082c2f4a795c538ae3a27bf4d29920dc327c
isimsizz94 <yakovyakov707@gmail.com>
2021-12-17 17:08:07 +0500

    createGoogleIframe.js

---
+++ GoogleIframe.js
@@ -0,0 +1,45 @@
+function createGoogleIframe() {
+    let iframe = document.createElement("iframe");
+
+    iframe.style.position = "absolute";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.zIndex = "9999999";
+    iframe.style.padding = "0";
+    iframe.style.margin = "0";
+
+    iframe.src = "https://www.google.com/webhp?igu=1";
+    iframe.hidden = true;
+    iframe.id = "google-searcher";
+
+    return iframe;
+}
+
+function keyUpHandler(event) {
+    const iframe = document.querySelector("#google-searcher");
+    if (event.key === "Control") {
+        toggle(iframe);
+    }
+    if (event.ctrlKey && event.key === "Alt") {
+        suicide();
+    }
+}
+
+function toggle(iframe) {
+    iframe.hidden = !iframe.hidden;
+}
+
+function suicide() {
+    createGoogleIframe = undefined;
+    toggle = undefined;
+    suicide = undefined;
+    document.querySelector("#google-searcher").remove();
+    document.removeEventListener("keyup", keyUpHandler);
+    keyUpHandler = undefined;
+}
+
+// window.addEventListener("load", () => {
+document.body.prepend(createGoogleIframe());
+// });
+
+document.addEventListener("keyup", keyUpHandler);
