574636ca0d26b41ed2a8ab039d4de2875c30f002
isimsizz94 <yakovyakov707@gmail.com>
2021-12-17 17:55:34 +0500

    createGoogleIframe.js

--- GoogleIframe.js
+++ GoogleIframe.js
@@ -1,12 +1,18 @@
 function createGoogleIframe() {
     let iframe = document.createElement("iframe");
 
-    iframe.style.position = "absolute";
-    iframe.style.width = "100%";
-    iframe.style.height = "100%";
-    iframe.style.zIndex = "9999999";
-    iframe.style.padding = "0";
-    iframe.style.margin = "0";
+    Object.assign(iframe.style, {
+        position: "absolute",
+        width: "50%",
+        height: "100%",
+        zIndex: "9999999",
+        padding: "0",
+        margin: "0",
+        frameBorder: "1",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
+    });
 
     iframe.src = "https://www.google.com/webhp?igu=1";
     iframe.hidden = true;
@@ -16,7 +22,6 @@ function createGoogleIframe() {
 }
 
 function keyUpHandler(event) {
-    const iframe = document.querySelector("#google-searcher");
     if (event.key === "Control") {
         toggle(iframe);
     }
@@ -29,15 +34,28 @@ function toggle(iframe) {
     iframe.hidden = !iframe.hidden;
 }
 
+function hide(iframe){
+    iframe.hidden = true;
+}
+function show(iframe){
+    iframe.hidden = false;
+}
+
 function suicide() {
     createGoogleIframe = undefined;
     toggle = undefined;
     suicide = undefined;
     document.querySelector("#google-searcher").remove();
-    document.removeEventListener("keyup", keyUpHandler);
+    window.removeEventListener("keyup", keyUpHandler);
     keyUpHandler = undefined;
 }
 
-document.body.prepend(createGoogleIframe());
 
-document.addEventListener("keyup", keyUpHandler);
+
+let iframe = createGoogleIframe();
+document.body.prepend(iframe);
+
+document.addEventListener("click", event => {
+    hide(iframe);
+});
+window.addEventListener("keyup", keyUpHandler);
